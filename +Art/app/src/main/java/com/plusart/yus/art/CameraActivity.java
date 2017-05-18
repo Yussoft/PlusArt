8079db67a04db1b4b4cf47114ccb06510c1f3753
Yussoft <jsdc94@gmail.com>
2017-05-18 12:10:48 +0200

    First Commit with empty Project

---
+++ +Art/app/src/main/java/com/plusart/yus/art/CameraActivity.java
@@ -0,0 +1,13 @@
+package com.plusart.yus.art;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
