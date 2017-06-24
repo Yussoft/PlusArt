14ebe660573643206d52ca8f559c2e753131f01a
Yussoft <jsdc94@gmail.com>
2017-06-24 09:18:50 +0200

    Nuevo proyecto Android, PlusArt

---
+++ PlusArt/app/src/main/java/com/plusart/yus/plusart/MainActivity.java
@@ -0,0 +1,13 @@
+package com.plusart.yus.plusart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
